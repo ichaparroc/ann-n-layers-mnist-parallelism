6ec97e15de603698bbd0d8cacf625093b53b83e1
ichaparroc <ichaparroc@gmail.com>
2018-08-19 11:25:15 -0500

    añadiendo test

--- main.cpp
+++ main.cpp
@@ -43,14 +43,20 @@ int ReverseInt(int i)
 
 int main(int argc,char* argv[])
 {
+
+	if(argc==1) cout<<argv[0]<<" n_layers [layers] n_epochs alpha";
+
 	cout.precision(8);
 
 	//internal variables
-	int n_images;
-	double error,instance_error;
-	//file read variables
+	int n_images_train;
+	int n_images_test;
+	double error_train,error_test,instance_error;
+
+	//read binary files variable
 	int magic_number;
 	unsigned char temp;
+
 
 	cout<<" Artificial Neural Network with n-layers";
 	cout<<endl<<"========================================="<<endl<<endl;
@@ -72,83 +78,164 @@ int main(int argc,char* argv[])
 	double alpha=strtod(argv[n_layers+3],NULL);
 	if(DBG) cout<<endl<<"alpha = "<<alpha;
 
-	//read MNIST input
-	double **input;
-	ifstream file_in("train-images.idx3-ubyte",ios::binary);
-	if(file_in.is_open())
+	//read MNIST input_train
+	double **input_train;
+
+	ifstream file_in_train("train-images.idx3-ubyte",ios::binary);
+	if(file_in_train.is_open())
 	{
 		int n_rows=0;
 		int n_cols=0;
 
-		file_in.read((char*)&magic_number,sizeof(magic_number));
+		file_in_train.read((char*)&magic_number,sizeof(magic_number));
 		magic_number=ReverseInt(magic_number);
-		if(DBG) cout<<endl<<"magic_number in input = "<<magic_number;
+		if(DBG) cout<<endl<<"magic_number in input_train = "<<magic_number;
 
-		file_in.read((char*)&n_images,sizeof(n_images));
-		n_images=ReverseInt(n_images);
-		if(DBG) cout<<endl<<"n_images in input = "<<n_images;
+		file_in_train.read((char*)&n_images_train,sizeof(n_images_train));
+		n_images_train=ReverseInt(n_images_train);
+		if(DBG) cout<<endl<<"n_images_train in input_train = "<<n_images_train;
 
-		input = new double*[n_images];
-		for(int i=0;i<n_images;i++)
-			input[i] = new double[layer[0]];
+		input_train = new double*[n_images_train];
+		for(int i=0;i<n_images_train;i++)
+			input_train[i] = new double[layer[0]];
 
-		file_in.read((char*)&n_rows,sizeof(n_rows));
+		file_in_train.read((char*)&n_rows,sizeof(n_rows));
 		n_rows= ReverseInt(n_rows);
-		if(DBG) cout<<endl<<"n_rows in input = "<<n_rows;
+		if(DBG) cout<<endl<<"n_rows in input_train = "<<n_rows;
 
-		file_in.read((char*)&n_cols,sizeof(n_cols));
+		file_in_train.read((char*)&n_cols,sizeof(n_cols));
 		n_cols= ReverseInt(n_cols);
-		if(DBG) cout<<endl<<"n_cols in input = "<<n_cols;
+		if(DBG) cout<<endl<<"n_cols in input_train = "<<n_cols;
 
-		for(int i=0;i<n_images;i++)
+		for(int i=0;i<n_images_train;i++)
 			for(int r=0;r<n_rows;r++)
 				for(int c=0;c<n_cols;c++)
 				{
-					file_in.read((char*)&temp,sizeof(temp));
-					input[i][(n_rows*r)+c]=(double)temp/255.0;
+					file_in_train.read((char*)&temp,sizeof(temp));
+					input_train[i][(n_rows*r)+c]=(double)temp/255.0;
 				}
-		file_in.close();
-		printf("\nSuccess Reading Input Data");
+		file_in_train.close();
+		printf("\nSuccess Reading input_train Data");
 	}
 	else
 	{
-		cout<<endl<<"Can not read input data ... !!!"<<endl;
+		cout<<endl<<"Can not read input_train data ... !!!"<<endl;
 		throw;
 	}
 
-	//read MNIST output
-	double **output;
-	ifstream file_out("train-labels.idx1-ubyte",ios::binary);
-	if (file_out.is_open())
+	//read MNIST output_train
+	double **output_train;
+	ifstream file_out_train("train-labels.idx1-ubyte",ios::binary);
+	if (file_out_train.is_open())
 	{
 
-		file_out.read((char*)&magic_number,sizeof(magic_number));
+		file_out_train.read((char*)&magic_number,sizeof(magic_number));
 		magic_number=ReverseInt(magic_number);
-		if(DBG) cout<<endl<<"magic_number in input = "<<magic_number;
+		if(DBG) cout<<endl<<"magic_number in output_train = "<<magic_number;
 
-		file_out.read((char*)&n_images,sizeof(n_images));
-		n_images=ReverseInt(n_images);
-		if(DBG) cout<<endl<<"n_images in input = "<<n_images;
+		file_out_train.read((char*)&n_images_train,sizeof(n_images_train));
+		n_images_train=ReverseInt(n_images_train);
+		if(DBG) cout<<endl<<"n_images_train in output_train = "<<n_images_train;
 
-		output = new double*[n_images];
-		for(int i=0;i<n_images;i++)
+		output_train = new double*[n_images_train];
+		for(int i=0;i<n_images_train;i++)
 		{
-			output[i] = new double[layer[n_layers-1]];
+			output_train[i] = new double[layer[n_layers-1]];
 			for(int j=0;j<layer[n_layers-1];j++)
-				output[i][j]=0;
+				output_train[i][j]=0;
 		}
 
-		for(int i=0;i<n_images;i++)
+		for(int i=0;i<n_images_train;i++)
 		{
-			file_out.read((char*)&temp,sizeof(temp));
-			output[i][(int)temp]=1;
+			file_out_train.read((char*)&temp,sizeof(temp));
+			output_train[i][(int)temp]=1;
 		}
-		file_out.close();
-		printf("\nSuccess Reading Output Data");
+		file_out_train.close();
+		printf("\nSuccess Reading output_train Data");
 	}
 	else
 	{
-		cout<<endl<<"Can not read output data ... !!!"<<endl;
+		cout<<endl<<"Can not read output_train data ... !!!"<<endl;
+		throw;
+	}
+	//read MNIST input_test
+	double **input_test;
+
+	ifstream file_in_test("t10k-images.idx3-ubyte",ios::binary);
+	if(file_in_test.is_open())
+	{
+		int n_rows=0;
+		int n_cols=0;
+
+		file_in_test.read((char*)&magic_number,sizeof(magic_number));
+		magic_number=ReverseInt(magic_number);
+		if(DBG) cout<<endl<<"magic_number in input_test = "<<magic_number;
+
+		file_in_test.read((char*)&n_images_test,sizeof(n_images_test));
+		n_images_test=ReverseInt(n_images_test);
+		if(DBG) cout<<endl<<"n_images_test in input_test = "<<n_images_test;
+
+		input_test = new double*[n_images_test];
+		for(int i=0;i<n_images_test;i++)
+			input_test[i] = new double[layer[0]];
+
+		file_in_test.read((char*)&n_rows,sizeof(n_rows));
+		n_rows= ReverseInt(n_rows);
+		if(DBG) cout<<endl<<"n_rows in input_test = "<<n_rows;
+
+		file_in_test.read((char*)&n_cols,sizeof(n_cols));
+		n_cols= ReverseInt(n_cols);
+		if(DBG) cout<<endl<<"n_cols in input_test = "<<n_cols;
+
+		for(int i=0;i<n_images_test;i++)
+			for(int r=0;r<n_rows;r++)
+				for(int c=0;c<n_cols;c++)
+				{
+					file_in_test.read((char*)&temp,sizeof(temp));
+					input_test[i][(n_rows*r)+c]=(double)temp/255.0;
+				}
+		file_in_test.close();
+		printf("\nSuccess Reading input_test Data");
+	}
+	else
+	{
+		cout<<endl<<"Can not read input_test data ... !!!"<<endl;
+		throw;
+	}
+
+	//read MNIST output_test
+	double **output_test;
+	ifstream file_out_test("t10k-labels.idx1-ubyte",ios::binary);
+	if (file_out_test.is_open())
+	{
+
+		file_out_test.read((char*)&magic_number,sizeof(magic_number));
+		magic_number=ReverseInt(magic_number);
+		if(DBG) cout<<endl<<"magic_number in output_train = "<<magic_number;
+
+		file_out_test.read((char*)&n_images_test,sizeof(n_images_test));
+		n_images_test=ReverseInt(n_images_test);
+		if(DBG) cout<<endl<<"n_images_test in output_train = "<<n_images_test;
+
+		output_test = new double*[n_images_test];
+		for(int i=0;i<n_images_test;i++)
+		{
+			output_test[i] = new double[layer[n_layers-1]];
+			for(int j=0;j<layer[n_layers-1];j++)
+				output_test[i][j]=0;
+		}
+
+		for(int i=0;i<n_images_test;i++)
+		{
+			file_out_test.read((char*)&temp,sizeof(temp));
+			output_test[i][(int)temp]=1;
+		}
+		file_out_test.close();
+		printf("\nSuccess Reading output_test Data");
+	}
+	else
+	{
+		cout<<endl<<"Can not read output_test data ... !!!"<<endl;
 		throw;
 	}
 
@@ -164,8 +251,8 @@ int main(int argc,char* argv[])
 		b[i] = new double[layer[i+1]];
 		for(int j=0;j<layer[i+1];j++)
 		{
-			b[i][j]=((double)rand()/(RAND_MAX));
-			//if(DBG) cout<<endl<<"b["<<i<<"]["<<j<<"] = "<<b[i][j];
+			b[i][j]=((double)rand()/(RAND_MAX))/n_images_train;
+			if(DBG) cout<<endl<<"b["<<i<<"]["<<j<<"] = "<<b[i][j];
 		}
 	}
 	for(int i=0;i<n_layers-1;i++)
@@ -176,7 +263,7 @@ int main(int argc,char* argv[])
 			w[i][j] = new double[layer[i+1]];
 			for(int k=0;k<layer[i+1];k++)
 			{
-				w[i][j][k]=((double)rand()/(RAND_MAX));
+				w[i][j][k]=((double)rand()/(RAND_MAX))/n_images_train;
 				//if(DBG) cout<<endl<<"w["<<i<<"]["<<j<<"]["<<k<<"] = "<<w[i][j][k];
 			}
 		}
@@ -190,13 +277,13 @@ int main(int argc,char* argv[])
 	//execution in epochs
 	for(int epoch=0;epoch<n_epoch;epoch++)
 	{
-		error=0.0;
-		//for every instance
-		//for(int instance=0;instance<n_images;instance++)
-		for(int instance=0;instance<1;instance++)
+		error_train=0.0;
+		//for every instance in train
+		for(int instance=0;instance<n_images_train;instance++)
+		//for(int instance=0;instance<1;instance++)
 		{
-			//forward propagation
-			a[0]=input[instance];
+			//forward propagation with TRAIN DATA
+			a[0]=input_train[instance];
 			for(int i=0;i<n_layers-1;i++)
 				for(int j=0;j<layer[i+1];j++)
 				{
@@ -206,18 +293,19 @@ int main(int argc,char* argv[])
 					a[i+1][j]=act(z);
 					if(DBG) cout<<endl<<"a["<<i+1<<"]["<<j<<"] = "<<a[i+1][j];
 				}
+			if(DBG){cout<<endl<<"target = "; for(int i=0;i<10;i++) cout<<output_train[instance][i];}
 
-			//calculate error
-			instance_error=mse(output[instance],a[n_layers-1],layer[n_layers-1]);
-			if(DBG) cout<<endl<<"instance error "<<instance<<" = "<<instance_error;
-			error+=instance_error;
+			//calculate error_train
+			instance_error=mse(output_train[instance],a[n_layers-1],layer[n_layers-1]);
+			if(DBG) cout<<endl<<"instance error_train "<<instance<<" = "<<instance_error;
+			error_train+=instance_error;
 
 			//backward propagation
 			//for the last layer
 			int m=n_layers-2;
 			for(int i=0;i<layer[m+1];i++)
 			{
-				d_b[m][i]=derror(output[instance][i],a[m+1][i])*dact(a[m+1][i]);
+				d_b[m][i]=derror(output_train[instance][i],a[m+1][i])*dact(a[m+1][i]);
 				if(DBG) cout<<endl<<"d_b["<<m<<"]["<<i<<"] = "<<d_b[m][i];
 			}
 
@@ -242,9 +330,35 @@ int main(int argc,char* argv[])
 						w[m][i][j]-=alpha*d_b[m][j]*a[m][i];
 				}
 		}
-		cout<<endl<<"Error en epoca "<<epoch<<": "<<error;
+		cout<<endl<<"Error en epoca para entrenamiento"<<epoch<<": "<<error_train;
+
+		error_test=0.0;
+		//for every instance in test
+		for(int instance=0;instance<n_images_test;instance++)
+		//for(int instance=0;instance<1;instance++)
+		{
+			//forward propagation with TEST DATA
+			a[0]=input_test[instance];
+			for(int i=0;i<n_layers-1;i++)
+				for(int j=0;j<layer[i+1];j++)
+				{
+					z=b[i][j];
+					for(int k=0;k<layer[i];k++)
+						z+=a[i][k]*w[i][k][j];
+					a[i+1][j]=act(z);
+					if(DBG) cout<<endl<<"a["<<i+1<<"]["<<j<<"] = "<<a[i+1][j];
+				}
+			if(DBG){cout<<endl<<"target = "; for(int i=0;i<10;i++) cout<<output_test[instance][i];}
+
+			//calculate error_test
+			instance_error=mse(output_test[instance],a[n_layers-1],layer[n_layers-1]);
+			if(DBG) cout<<endl<<"instance error_test"<<instance<<" = "<<instance_error;
+			error_test+=instance_error;
+		}
+
+		cout<<endl<<"Error en epoca para test"<<epoch<<": "<<error_train;
+
 	}
 
 	return 0;
 }
-
